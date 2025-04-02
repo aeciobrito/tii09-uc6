b380dd67b2265a2cf194ffcc2f6b116838ab47c7
aecio <aeciobrito@gmail.com>
2025-04-01 22:03:11 -0300

    Aula 10 - Heranca e Polimorfismo

---
+++ 10/rpg.js
@@ -0,0 +1,30 @@
+class Personagem {
+    _nome;
+    _forca;
+    _pontosDeVida;
+    _vivo; // valor booleano
+
+    atacar() {
+
+    }
+
+    tomarDano() {
+
+    }
+
+    recuperarVida() {
+
+    }
+
+    morrer() {
+
+    }
+}
+
+class Guerreiro {
+    // Ao tomar dano, é reduzido em 5 por conta do escudo;
+}
+
+class Mago {
+    // Ao atacar, reduz 5 da própria vida, para fazer um ataque
+}
