167e7daa812f5cfd862167ffe76cae3ccd5aff90
aecio <aeciobrito@gmail.com>
2025-04-01 22:16:44 -0300

    Exemplo acrescentado para heranca e polimorfismo

--- 10/rpg.js
+++ 10/rpg.js
@@ -4,12 +4,19 @@ class Personagem {
     _pontosDeVida;
     _vivo; // valor booleano
 
+    constructor(nome, forca, vida) {
+        this._nome = nome;
+        this._forca = forca;
+        this._pontosDeVida = vida;
+        this._vivo = true;
+    }
+
     atacar() {
 
     }
 
     tomarDano() {
-
+        console.log("Tomou dano de X");
     }
 
     recuperarVida() {
@@ -21,10 +28,17 @@ class Personagem {
     }
 }
 
-class Guerreiro {
+class Guerreiro extends Personagem {
     // Ao tomar dano, é reduzido em 5 por conta do escudo;
+    tomarDano() {
+        super.tomarDano();
+        console.log("Mas, esse dano foi reduzido em Y");
+    }
 }
 
-class Mago {
+class Mago extends Personagem {
     // Ao atacar, reduz 5 da própria vida, para fazer um ataque
 }
+
+let personagem1 = new Mago("Connan", 200, 250);
+personagem1.tomarDano();
